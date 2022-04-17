1ae329044b480c0e40cf4e84ba9f6bd38675ed22
PuqiAR <101029101+PuqiAR@users.noreply.github.com>
2022-04-17 11:51:12 +0800

    新增了液晶屏上的信息

--- main.ts
+++ main.ts
@@ -6,10 +6,10 @@ function 左转 () {
     neZha.setMotorSpeed(neZha.MotorList.M1, 马达速度 / 4)
     neZha.setMotorSpeed(neZha.MotorList.M2, 马达速度 + 20)
 }
-let 往左走还是往右走 = 0
 let 马达速度 = 0
 马达速度 = -50
 let 障碍物距离 = 0
+let 往左走还是往右走 = "False"
 PlanetX_Display.showUserText(1, "Starting......")
 PlanetX_Display.oledClear()
 basic.forever(function () {
@@ -18,10 +18,11 @@ basic.forever(function () {
         左转()
         basic.pause(2000)
         basic.pause(500)
+        往左走还是往右走 = "True"
     } else {
         neZha.setMotorSpeed(neZha.MotorList.M1, 马达速度)
         neZha.setMotorSpeed(neZha.MotorList.M2, 马达速度 + -10)
-        往左走还是往右走 = 3
+        往左走还是往右走 = "False"
     }
     PlanetX_Display.showUserText(2, "Line:" + 障碍物距离)
     PlanetX_Display.showUserText(3, "Speed:" + 马达速度)
